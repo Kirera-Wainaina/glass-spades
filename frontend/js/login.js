8e3368aeafb71614d22cc4cc231060368d4bba2e
Kirera Wainaina <richardwainainak@gmail.com>
2021-05-07 08:20:18 +0300

    Show error message upon unsuccessful login

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -10,4 +10,28 @@ function loginUser(formdata) {
     const xhr = new XMLHttpRequest();
     xhr.open("POST", "/api/login/loginUser");
     xhr.send(formdata);
+
+    xhr.onreadystatechange = function () {
+	if (this.readyState == 4 && this.response == "error") {
+	    showError()
+	} else if (this.readyState == 4 && this.response == "success") {
+	    location.reload();
+	}
+    }
 }
+
+function showError() {
+    const errorMsg = document.getElementById("email-error");
+    errorMsg.classList.remove("hide-error");
+    errorMsg.classList.add("show-error")
+}
+
+// function showLoadingPage() {
+//     const loadingPage = document.getElementById("loading-page");
+//     loadingPage.style.display = "flex";
+// }
+
+// function hideLoadingPage() {
+//     const loadingPage = document.getElementById("loading-page");
+//     loadingPage.style.display = "none";
+// }
