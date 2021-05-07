3b38c6445a62552b82b481b26ffd2e9cd9a8e898
Kirera Wainaina <richardwainainak@gmail.com>
2021-05-07 07:41:30 +0300

    Confirm email exists upon login

--- api/login.js
+++ api/login.js
@@ -1,13 +1,32 @@
 const Busboy = require("busboy");
+const bcrypt = require("bcrypt")
+
+const database = require("../database/models");
 
 function loginUser(request, response) {
     const busboy = new Busboy({ headers: request.headers });
+    const credentials = {};
 
     busboy.on("field", (fieldname, value) => {
-	console.log(`${fieldname}: ${value}`)
+	credentials[fieldname] = value;
     })
 
+    busboy.on("finish", () => {
+	console.log(credentials["email"])
+	database.User.exists({ "email": credentials["email"] })
+	    .then(emailExists => {
+		if (emailExists) {
+		    confirmPassword(password, response)
+		}
+	    })
+	    .catch(error => console.error())
+    });
+
     request.pipe(busboy)
+}
+
+function confirmPassword(password, response) {
+    
 }
 
 exports.loginUser = loginUser;
