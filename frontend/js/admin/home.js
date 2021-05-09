3d33d6064f68bd8e3cc9d0335cf98b2fdb8f83ea
Kirera Wainaina <richardwainainak@gmail.com>
2021-05-09 09:02:15 +0300

    Redirect back to login page is theres no cookie
    Other than helping keep people out of the admin page, it'll help us
    know if we havent logged in.

--- frontend/js/admin/home.js
+++ frontend/js/admin/home.js
@@ -1,1 +1,11 @@
-console.log("I'm here")
+window.addEventListener("load", () => {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "/api/signup/checkLogin");
+    xhr.send()
+
+    xhr.onreadystatechange = function() {
+	if (xhr.readyState == 4 && xhr.response != "/admin/home") {
+	    location.href = "/login"
+	}
+    }
+})
