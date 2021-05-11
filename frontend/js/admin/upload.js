8913f0903c98f48cffa3e0232b72ca6536431db6
Kirera Wainaina <richardwainainak@gmail.com>
2021-05-11 09:42:23 +0300

    Dynamically generate all the buttons and sections
    For the first part of the upload form

--- frontend/js/admin/upload.js
+++ frontend/js/admin/upload.js
@@ -6,22 +6,60 @@ window.addEventListener("load", () => {
     xhr.onreadystatechange = function() {
 	if (this.readyState == 4) {
 	    const model = JSON.parse(this.response);
-	    createMandate(model["Mandate"]);
+
+	    const containerFragment = createContainers(model);
+	    const parentContainer = document.querySelector(".page");
+	    parentContainer.appendChild(containerFragment)
+
+	    // Take footer to bottom position
+	    parentContainer.appendChild(document.querySelector("footer"))
 	}
     }
 });
 
-function createMandate(data) {
+function createContainers(model) {
+    const keys = Object.keys(model);
+    const fragment = new DocumentFragment();
+    keys.forEach(key => {
+	const div = document.createElement("div");
+	div.id = `${key}-container`;
+	div.classList.add("container");
+	div.appendChild(createLabel(key));
+	div.appendChild(createButtons(key, model[key]));
+	fragment.append(div)
+    })
+    return fragment
+}
+
+function createLabel(key) {
+    const p = document.createElement("p");
+    p.classList.add("label");
+    p.id = `${key}-label`;
+    p.innerText = key;
+    return p
+}
+
+function createButtons(key, data) {
     const fragment = new DocumentFragment();
     data.forEach(type => {
 	const button = document.createElement("button");
 	button.value = type;
 	button.type = "button";
-	button.name = "mandate";
+	button.name = key;
 	button.innerText = type
 	fragment.appendChild(button);
     })
-
-    const mandateContainer = document.getElementById("mandate-container");
-    mandateContainer.appendChild(fragment);
+    return fragment
 }
+
+const containerList = document.getElementsByClassName("container");
+for (let i = 0; i < containerList.length; i++) {
+    containerList[i].addEventListener("click", (event) => {
+	console.log(`${event.target.name}: ${event.target.value}`)
+    })
+}
+
+const page = document.querySelector(".page");
+page.addEventListener("click", (event) => {
+    console.log(`${event.target.name}: ${event.target.value}`)
+})
