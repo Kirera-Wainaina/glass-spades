ecaa0c8924830b911a0761b033cb2ee3dc04e9e7
Kirera Wainaina <richardwainainak@gmail.com>
2021-05-11 06:05:16 +0300

    Send a request for model data
    The model data will guide the information the upload page will need

---
+++ frontend/js/admin/upload.js
@@ -0,0 +1,11 @@
+window.addEventListener("load", () => {
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", "/api/admin/upload/sendModelData");
+    xhr.send();
+
+    xhr.onreadystatechange = function() {
+	if (this.readyState == 4) {
+	    console.log(this.response)
+	}
+    }
+});
