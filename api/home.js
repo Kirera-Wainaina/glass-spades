9fcd18bd10d3c319a8d63bba9284603d59834e34
Kirera Wainaina <richardwainainak@gmail.com>
2021-06-02 18:10:19 +0300

    After getting overview photos get the house prices

--- api/home.js
+++ api/home.js
@@ -3,6 +3,19 @@ const db = require("../database/models");
 async function getListings(request, response) {
     const overviews = await db.Image.find({ name: /-0.webp/ })
     console.log(overviews)
+    Promise.all(overviews.map(overview => {
+	return new Promise((resolve, reject) => {
+	    db.Listing.findById(overview.listingId, (error, docs) => {
+		if (error) reject(error);
+		resolve({
+		    "imageSrc": overview.link,
+		    "price": docs.Price
+		});
+	    })
+	})
+    }))
+	.then(details => console.log(details))
+	.catch(error => console.error())
 }
 
 exports.getListings = getListings;
