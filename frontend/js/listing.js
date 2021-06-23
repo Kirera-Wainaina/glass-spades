5f56b9d6c82398561a4079807ad8a6fc90cf9d05
Kirera Wainaina <richardwainainak@gmail.com>
2021-06-23 21:00:41 +0300

    Console.log details in listings page

--- frontend/js/listing.js
+++ frontend/js/listing.js
@@ -108,6 +108,7 @@ function setTitle(details) {
 }
 
 async function createBody(details) {
+    console.log(details);
     const fragment = new DocumentFragment();
     const page = document.querySelector(".page");
     const form = document.querySelector("form");
