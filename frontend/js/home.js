b712997fe4bc11ce01ba216be813107b0b00d366
Kirera Wainaina <richardwainainak@gmail.com>
2021-06-27 12:21:53 +0300

    Phone menu shown when menu button is clicked

--- frontend/js/home.js
+++ frontend/js/home.js
@@ -82,7 +82,6 @@ function createIcon(value, icon) {
     return div
 }
 
-// window.addEventListener("DOMContentLoaded", handleMenu);
 const mediaQueryResult = window.matchMedia("(max-width: 500px)");
 mediaQueryResult.addEventListener("change", handleMenu);
 handleMenu(mediaQueryResult);
@@ -93,8 +92,20 @@ function handleMenu(mediaQueryResult) {
     if (mediaQueryResult.matches) {
 	menu.style.display = "none";
 	menuButton.style.display = "block";
+	menuButton.addEventListener("click", showPhoneMenu);
     } else {
 	menu.style.display = "flex";
 	menuButton.style.display = "none";
+	menuButton.removeEventListener("click", showPhoneMenu);
+    }
+}
+
+function showPhoneMenu(event) {
+    const menu = document.getElementById("menu");
+    if (menu.style.display == "none") {
+	menu.style.display = "flex";
+	menu.classList.add("phone-menu");
+    } else {
+	menu.style.display  = "none";
     }
 }
