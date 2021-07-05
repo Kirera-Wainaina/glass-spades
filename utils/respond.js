ca07503b521d596dd4ff1dda8f5f2c787770436d
Kirera Wainaina <richardwainainak@gmail.com>
2021-07-05 20:14:38 +0300

    Create function to handle JSON response

--- utils/respond.js
+++ utils/respond.js
@@ -1,6 +1,12 @@
-function handleTextResponse(text, response) {
+function handleTextResponse(response, text) {
     response.writeHead(200, { "content-type": "text/plain"})
 	.end(text)
 }
 
+function handleJSONResponse(response, data) {
+    response.writeHead(200, { "content-type": "application/json" })
+	.end(JSON.stringify(data));
+}
+
 exports.handleTextResponse = handleTextResponse;
+exports.handleJSONResponse = handleJSONResponse;
