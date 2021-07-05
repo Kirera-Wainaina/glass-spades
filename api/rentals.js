6b3c4a7179f6732e56de7bb5998589dae80461da
Kirera Wainaina <richardwainainak@gmail.com>
2021-07-05 20:06:36 +0300

    Send rental data to the frontend

--- api/rentals.js
+++ api/rentals.js
@@ -1,4 +1,5 @@
 const db = require("../database/models");
+const respond = require("../utils/respond");
 
 async function getRentals(request, response) {
     const rentals = await db.Listing.find({ Mandate: "Rent" },
@@ -8,8 +9,20 @@ async function getRentals(request, response) {
 					      Bedrooms: 1,
 					      Bathrooms: 1
 					  });
-    
-    console.log(rentals)
+    if (rentals.length) {
+	const data = await Promise.all(rentals.map(rental => {
+	    return new Promise(async (resolve, reject) => {
+		const imageData = await db.Image.findOne({ listingId: rental._id,
+							   position: 0 });
+		// _doc is to bring only the rental properties without the object's
+		resolve({...rental._doc, imgSrc: imageData.link});
+	    })
+	}));
+	respond.handleJSONResponse(response, data);
+    } else {
+	respond.handleTextResponse("fail", response);
+    }
+    console.log(data)
 }
 
 exports.getRentals = getRentals;
