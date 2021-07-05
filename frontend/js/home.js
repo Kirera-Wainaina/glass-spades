9aadd4b965bf0974d88c6a32005c9eec4d288faa
Kirera Wainaina <richardwainainak@gmail.com>
2021-07-05 19:24:47 +0300

    Remove the functions determine menu and place in general js

--- frontend/js/home.js
+++ frontend/js/home.js
@@ -81,31 +81,4 @@ function createIcon(value, icon) {
 
     return div
 }
-
-const mediaQueryResult = window.matchMedia("(max-width: 500px)");
-mediaQueryResult.addEventListener("change", handleMenu);
-handleMenu(mediaQueryResult);
-
-function handleMenu(mediaQueryResult) {
-    const menu = document.getElementById("menu");
-    const menuButton = document.getElementById("menu-button");
-    if (mediaQueryResult.matches) {
-	menu.style.display = "none";
-	menuButton.style.display = "block";
-	menuButton.addEventListener("click", showPhoneMenu);
-    } else {
-	menu.style.display = "flex";
-	menuButton.style.display = "none";
-	menuButton.removeEventListener("click", showPhoneMenu);
-    }
-}
-
-function showPhoneMenu(event) {
-    const menu = document.getElementById("menu");
-    if (menu.style.display == "none") {
-	menu.style.display = "flex";
-	menu.classList.add("phone-menu");
-    } else {
-	menu.style.display  = "none";
-    }
-}
+ 
