0c44ce53bb901f91eec0abbb408e942a9142fa52
Kirera Wainaina <richardwainainak@gmail.com>
2021-07-05 19:39:01 +0300

    Send request for rentals

--- frontend/js/rentals.js
+++ frontend/js/rentals.js
@@ -1,1 +1,11 @@
-console.log("rentals js");
+getRentals();
+
+function getRentals() {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "/api/rentals/getRentals");
+    xhr.send();
+
+    xhr.onreadystatechange = function() {
+	console.log(this.response);
+    }
+}
