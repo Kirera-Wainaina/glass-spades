23bf216d794a3c02588beacb4c322228124970d3
Kirera Wainaina <richardwainainak@gmail.com>
2021-07-15 14:23:07 +0300

    Remove shared functions and place them in index-utils

--- index.js
+++ index.js
@@ -9,12 +9,13 @@ const qs = require("querystring");
 const dotenv = require("dotenv");
 const puppeteer = require("puppeteer");
 const MIMES = require("./utils/MIMETypes.js");
+const indexUtils = require("./index-utils.js");
 let wsEndpoint, routeCache;
 
 dotenv.config()
 
-const httpPort = 80;
-const port = 443;
+const httpPort = process.env.HTTP_PORT;
+const port = process.env.PORT;
 
 const options = {
     key: fs.readFileSync(`${process.env.CERTPATH}/privkey.pem`, "utf8"),
@@ -82,64 +83,6 @@ httpServer.on("error", error => {
 })
 
 
-//////////////////////////////////////////////////////////////////////////
-
-function readFileAndRespond(filePath, response, statusCode=null) {
-    fs.stat(filePath, (error, stats) => {
-	if (error) {
-	    handleError(error, response)
-	} else {
-	    const mime = MIMES.findMIMETypeFromExtension(path.extname(filePath));
-
-	    if (mime.split("/")[0] == "image") {
-		// images should not be compressed
-		response.writeHead(statusCode || 200, {
-		    "content-type": mime
-		})
-
-		fs.createReadStream(filePath)
-		    .pipe(response)
-	    } else {
-		response.writeHead(statusCode || 200, {
-		    "content-type": mime,
-		    "content-encoding": "gzip"
-		})
-		
-		fs.createReadStream(filePath)
-		    .pipe(zlib.createGzip())
-		    .pipe(response)
-	    }
-
-	}
-    })
-    
-}
-
-function handleError(error, response) {
-    if (error.code == "ENOENT") {
-	const filePath = "./frontend/html/error.html";
-	const errorCode = 404;
-	readFileAndRespond(filePath, response, errorCode)
-    } else {
-	console.log(error);
-	const filePath = "./frontend/html/error.html";
-	const errorCode = 404;
-	readFileAndRespond(filePath, response, errorCode)
-    }
-}
-
-
-function findTopDir(route) {
-    let dir = route;
-    let top;
-    while (dir != "/") {
-	if (path.dirname(dir) == "/") {
-	    top = dir;
-	}
-	dir = path.dirname(dir);
-    }
-    return top
-}
 
 async function serverSideRender(request, response) {
     const parsed_url = url.parse(request.url);
@@ -165,22 +108,4 @@ async function serverSideRender(request, response) {
 		.end(html)
 	}
     }
-}
-
-function createFilePath(urlPath) {
-    const cwd = ".";
-    const parsed_url = url.parse(urlPath);
-    let filePath;
-
-    if (urlPath == "/") {
-	filePath = `${cwd}/frontend/html/home.html`;
-    } else if (!path.extname(parsed_url.pathname) ){
-	// browser paths
-	filePath = `${cwd}/frontend/html${parsed_url.pathname}.html`;
-    } else {
-	// etc files e.g favicon
-	filePath = `${cwd}${parsed_url.pathname}`;
-    }
-
-    return filePath
 }
