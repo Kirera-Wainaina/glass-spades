d7a8d6912dcb7feb0674bd4f6e280eea0c007197
Kirera Wainaina <richardwainainak@gmail.com>
2021-07-15 14:24:57 +0300

    This version of index2.js does not have server side rendering

---
+++ index2.js
@@ -0,0 +1,75 @@
+const http2 = require("http2");
+const http = require("http");
+const url = require("url");
+const path = require("path");
+const fs = require("fs");
+
+const indexUtils = require("./index-utils.js");
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+const httpPort = 80;
+const port = 443;
+
+const options = {
+    key: fs.readFileSync(`${process.env.CERTPATH}/privkey.pem`, "utf8"),
+    cert: fs.readFileSync(`${process.env.CERTPATH}/fullchain.pem`, "utf8"),
+    ca: fs.readFileSync(`${process.env.CERTPATH}/chain.pem`, "utf8"),
+    allowHTTP1: true
+};
+
+
+const server = http2.createSecureServer(options);
+
+server.on("request", async (request, response) => {
+    console.log(`Date: ${new Date()}, Path: ${request.url} http: ${request.httpVersion}`)
+    const parsed_url = url.parse(request.url);
+    const cwd = ".";
+
+    if (request.url == "/") {
+	const filePath = indexUtils.createFilePath(request.url);
+	indexUtils.readFileAndRespond(filePath, response);
+    } else if (indexUtils.findTopDir(request.url) == "/api"){
+	// has to come before browser requests
+	try {
+	    const file = require(`${cwd}${path.dirname(request.url)}`);
+	    const execute = path.basename(request.url);
+	    file[execute](request, response);
+	} catch(error) {
+	    indexUtils.handleError(error, response);
+	}
+    } else if (!path.extname(parsed_url.pathname) ){
+	// browser paths
+	const filePath = indexUtils.createFilePath(request.url)
+	indexUtils.readFileAndRespond(filePath, response);
+    } else {
+	const filePath = indexUtils.createFilePath(request.url);
+	indexUtils.readFileAndRespond(filePath, response);
+    }
+});
+
+server.listen(port, () => console.log(`Listening on port ${port}`));
+
+////////////////////////////////////////////////////////////////////////
+
+const httpServer = http.createServer();
+
+httpServer.on("request", (request, response) => {
+    console.log("A request was received on HTTP server");
+    response
+	.writeHead(301, {
+	    "location": `${process.env.URL}${request.url}`
+	})
+	.end();
+    
+});
+
+httpServer.listen(httpPort, () =>
+    console.log(`Http server listening on port ${httpPort}`));
+
+httpServer.on("error", error => {
+    console.log(error);
+})
+
+
