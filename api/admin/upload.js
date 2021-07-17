ed7ed2c4192db565cb028f9f428dbcc2c028d28d
Kirera Wainaina <richardwainainak@gmail.com>
2021-07-17 09:42:06 +0300

    Add try catch statement to know if anything goes wrong

--- api/admin/upload.js
+++ api/admin/upload.js
@@ -5,6 +5,7 @@ const EventEmitter = require("events");
 const Busboy = require("busboy");
 
 const images = require("../../utils/images.js");
+const respond = require("../utils/respond.js");
 const db = require("../../database/models.js");
 
 class Emitter extends EventEmitter {};
@@ -51,45 +52,36 @@ function uploadListing(request, response) {
 		console.log(error)
 	    })
 	    .on("finish", async () => {
-		fileNames.push({ name, position: nameSplit[2] });
- 		// const file = await images.minifyImage(route);
-		// fs.unlink(route, error => console.error());
-		// const cloudFile = await images.saveImage(file[0].destinationPath);
-		// fs.unlink(file[0].destinationPath, error => console.error());
-		// const [ metadata ] = await images.getFileMetadata(cloudFile)
-		// const imageInfo = {
-		//     googleId: metadata["id"],
-		//     link: metadata["mediaLink"],
-		//     name: metadata["name"],
-		//     contentType: metadata["contentType"],
-		//     position: nameSplit[2]
-		// };
-		// imageMetadata.push(imageInfo);
-		// emitter.emit("uploaded", listing, imageMetadata, response);
-		if (fileNames.length == listing.imageNum) {
-		    const convertedFiles = await Promise.all(
-			fileNames.map(filename => images.minifyImage(
-			    path.join(imageFolder, "uploaded", filename.name))));
+		try {
+		    fileNames.push({ name, position: nameSplit[2] });
+		    if (fileNames.length == listing.imageNum) {
+			const convertedFiles = await Promise.all(
+			    fileNames.map(filename => images.minifyImage(
+				path.join(imageFolder,
+					  "uploaded",
+					  filename.name))));
 
-		    const cloudFiles = await Promise.all(
-			convertedFiles.map(convertedFile => images.saveImage(
-			    convertedFile[0].destinationPath)));
+			const cloudFiles = await Promise.all(
+			    convertedFiles.map(convertedFile => images.saveImage(
+				convertedFile[0].destinationPath)));
 
-		    const googleMetadata = await Promise.all(
-			cloudFiles.map(cloudFile => images.getFileMetadata(
-			    cloudFile)));
-		    const metadata = googleMetadata.map((data, index) => {
-			const [ itemMetadata ] = googleMetadata[index];
-			return itemMetadata
-		    });
-		    convertedFiles.forEach(file => fs.unlinkSync(
-			file[0].sourcePath));
-		    convertedFiles.forEach(file => fs.unlinkSync(
-			file[0].destinationPath));
+			const googleMetadata = await Promise.all(
+			    cloudFiles.map(cloudFile => images.getFileMetadata(
+				cloudFile)));
+			const metadata = googleMetadata.map((data, index) => {
+			    const [ itemMetadata ] = googleMetadata[index];
+			    return itemMetadata
+			});
+			convertedFiles.forEach(file => fs.unlinkSync(
+			    file[0].sourcePath));
+			convertedFiles.forEach(file => fs.unlinkSync(
+			    file[0].destinationPath));
 
-		    const listingId = saveListingToDB(listing);
-		    console.log(listingId);
-		    saveImageToDB(listingId, fileNames, metadata);
+			const listingId = saveListingToDB(listing);
+			await saveImageToDB(listingId, fileNames, metadata);
+		    }
+		} catch (error) {
+		    console.log(error);
 		}
 	    })
     })
@@ -103,52 +95,17 @@ function uploadListing(request, response) {
     request.pipe(busboy);
 }
 
-emitter.on("uploaded", async (listing, imageMetadata, response) => {
-
-    if (listing.imageNum == imageMetadata.length) {
-	listing["Location"] = {
-	    coordinates: [ listing.Longitude, listing.Latitude ]
-	};
-	const newListing = new db.Listing(listing);
-	await newListing.save()
-	console.log("Finished saving to db")
-	saveImageMetadata(imageMetadata, newListing._id)
-	    .then(results => {
-		response.writeHead(200, {
-		    "content-type": "text/plain",
-		})
-		    .end("success")
-	    })
-    }
-})
-
-function saveImageMetadata(imageMetadata, listingId) {
-
-    const promiseArray = imageMetadata.map(data => {
-	data["listingId"] = listingId;
-	const newImage = new db.Image(data);
-	return new Promise((resolve, reject) => {
-	    newImage.save()
-		.then(doc => resolve(doc))
-		.catch(error => console.error());
-	})
-    });
-
-    return Promise.all(promiseArray)
-}
-
 function saveListingToDB(listing) {
     listing["Location"] = {
 	coordinates: [ listing.Longitude, listing.Latitude ]
     };
     const newListing = new db.Listing(listing);
-    // await newListing.save()
-    console.log(newListing);
+    await newListing.save()
     return newListing._id
 }
 
-async function saveImageToDB(listingId, fileNames, metadata) {
-    await Promise.all(metadata.map(imageMeta => {
+function savesImageToDB(listingId, fileNames, metadata) {
+    return Promise.all(metadata.map(imageMeta => {
 	const position = fileNames.filter(
 	    fileName => `${fileName.name}.webp` == imageMeta.name)[0].position;
 	const imageModel = new db.Image({
@@ -159,8 +116,7 @@ async function saveImageToDB(listingId, fileNames, metadata) {
 	    name: imageMeta.name,
 	    contentType: imageMeta.contentType
 	});
-	console.log(imageModel)
-	// return imageModel.save();
+	return imageModel.save();
     }))
 }
 
