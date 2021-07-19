6b427e32f21910bd243a2169355eeed6788c82c4
Kirera Wainaina <richardwainainak@gmail.com>
2021-07-19 11:23:08 +0300

    Exclude archived listings from homepage

--- api/home.js
+++ api/home.js
@@ -2,7 +2,8 @@ const db = require("../database/models");
 const respond = require("../utils/respond");
 
 async function getListings(request, response) {
-    const featuredListings = await db.Listing.find({ Featured: true });
+    const featuredListings = await db.Listing.find({ Featured: true ,
+						     Archived: false });
 
     const details = await Promise.all(featuredListings.map(listing => {
 	return new Promise(async (resolve, reject) => {
