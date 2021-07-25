27c7e4b0e11375674edaa12164634d85138ba819
Kirera Wainaina <richardwainainak@gmail.com>
2021-07-25 10:50:55 +0300

    Run the google tag code only if location origin is glassspades.com

--- frontend/js/general.js
+++ frontend/js/general.js
@@ -1,9 +1,11 @@
 // <!-- Google Tag Manager -->
+if (location.origin == "https://glassspades.com") {
 (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-WRW5KFK');
+}
 // <!-- End Google Tag Manager -->
 
 const mediaQueryResult = window.matchMedia("(max-width: 500px)");
