042e126266cfd40c046bb5110200e670c8b5b748
Kirera Wainaina <richardwainainak@gmail.com>
2021-07-27 14:33:25 +0300

    Handle animation from element to element

--- frontend/js/home.js
+++ frontend/js/home.js
@@ -13,3 +13,19 @@ function getListings() {
 	}
     }
 }
+
+const offerContainer = document.querySelector("#offer-container");
+for (let i = 0; i < offerContainer.childElementCount; i++) {
+    offerContainer.children[i]
+	.addEventListener("animationend", handleNextAnimation)
+}
+
+function handleNextAnimation(event) {
+    const el = event.target;
+    el.classList.remove("offer-animation");
+    if (el.nextElementSibling) {
+	el.nextElementSibling.classList.add("offer-animation");
+    } else {
+	el.parentNode.firstElementChild.classList.add("offer-animation");
+    }
+}
