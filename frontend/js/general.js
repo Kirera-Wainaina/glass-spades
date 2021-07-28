75b10f535af654a8024d97399df131f9e584e46a
Kirera Wainaina <richardwainainak@gmail.com>
2021-07-28 12:22:57 +0300

    Prevent analytics if rendering on headless chromium

--- frontend/js/general.js
+++ frontend/js/general.js
@@ -1,5 +1,6 @@
 // <!-- Google Tag Manager -->
-if (location.origin == "https://glassspades.com") {
+if (location.origin == "https://glassspades.com"
+    && navigator.userAgent != "glassspades-headless-chromium") {
 (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
