57e5899a7d275537dac47f67997d2e2871f8b550
Kirera Wainaina <richardwainainak@gmail.com>
2021-11-28 09:32:30 +0300

    Move the GTM code to listing page
    It is probably not working because it is placed in general.js

--- frontend/js/listing.js
+++ frontend/js/listing.js
@@ -1,3 +1,14 @@
+// <!-- Google Tag Manager -->
+if (location.origin == "https://glassspades.com"
+    && navigator.userAgent != "glassspades-headless-chromium") {
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WRW5KFK');
+}
+// <!-- End Google Tag Manager -->
+
 var dataLayer = [];
 getListingDetails();
 getListingImages();
