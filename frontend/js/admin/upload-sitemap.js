b023b733d42831219235f6df2066422d28343ea7
Kirera Wainaina <richardwainainak@gmail.com>
2022-01-09 15:48:49 +0300

    Prevent form default when uploading

--- frontend/js/admin/upload-sitemap.js
+++ frontend/js/admin/upload-sitemap.js
@@ -1,6 +1,8 @@
-console.log("sitemap file")
-
 const form = document.querySelector("form");
 form.addEventListener("submit", event => {
     event.preventDefault();
+    console.log("submit")
+    const formdata = new FormData();
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", "/api/admin/upload-sitemap/up")
 });
