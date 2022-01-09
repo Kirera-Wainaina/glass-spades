ecd489e0db7fde207b53103fa67ffb4d763fc98f
Kirera Wainaina <richardwainainak@gmail.com>
2022-01-09 17:18:35 +0300

    Handle saving the sitemap file

--- api/admin/general-functions.js
+++ api/admin/general-functions.js
@@ -3,20 +3,21 @@ const path = require("path");
 
 const Busboy = require("busboy");
 const dotenv = require("dotenv");
+
+const respond = require("../../utils/respond.js");
 dotenv.config()
 
 function uploadSitemap(request, response) {
     const busboy = new Busboy({ headers: request.headers });
 
     busboy.on("file", (name, file) => {
-	// const filePath = path.join(__dirname, "xyx");
-	// console.log(process.env.SITEMAP)
 	file.pipe(fs.createWriteStream(process.env.SITEMAP))
 	    .on("error", error => console.log(error))
     })
 
-    busboy.on("close", () => {
-	console.log("done")
+    busboy.on("finish", () => {
+	console.log("called")
+	respond.handleTextResponse(response, "success");
     })
 
     request.pipe(busboy);
