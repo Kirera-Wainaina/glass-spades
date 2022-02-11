33b23698193d42904af64f6215e6cb20c22d0e39
Kirera Wainaina <richardwainainak@gmail.com>
2022-02-11 04:57:49 +0300

    Run node webserver from start.js

--- start.js
+++ start.js
@@ -1,16 +1,35 @@
 const { spawn } = require("child_process");
-
+const qs = require("querystring");
+const path = require("path");
 
 // setInterval(checkStatus, 120000);
 checkStatus();
 
 function checkStatus() {
-    checkNodeServer();
+    checkWebServer();
 }
 
-function checkNodeServer() {
+function checkWebServer() {
     const pgrep = spawn("pgrep", ["-l", "node"]);
-    pgrep.stdout.on("data", data => console.log(String(data)));
+    pgrep.stdout.on("data", data => {
+	const str = String(data)
+	const parsed = qs.parse(str, "\n", " ");
+	if (Object.keys(parsed).length == 1) {
+	    // with only 1 node process, it's the server that isn't running
+	    startWebServer();
+	}
+	console.log(parsed);
+    });
     pgrep.stderr.on("data", data => console.log(String(data)));
     pgrep.on("close", code => console.log("Pgrep just ran"));
+}
+
+function startWebServer() {
+    const indexPath = path.join(__dirname, "index2.js");
+    // const webServer = spawn("nohup", ["node", indexPath, "&"]);
+    const webServer = spawn("node", [indexPath]);
+    console.log(webServer.pid);
+    webServer.stdout.on("data", data => console.log(String(data)))
+    webServer.stderr.on("data", data => console.log(String(data)));
+    webServer.on("close", code => console.log("Web server closed"))
 }
