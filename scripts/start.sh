47b1c4a1b45238db59ae6ef3f065186b6dbfee12
Kirera Wainaina <richardwainainak@gmail.com>
2022-02-18 05:47:58 +0300

    Start server through a bash script

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-PATH=/sbin:/snap/bin:/bin:/usr/local/bin
 export CERTPATH=/etc/letsencrypt/live/glassspades.ddns.net
 export URL=https://glassspades.ddns.net
 export ENCRYPTED_ADMIN_PASSWORD=$2b$10$BjUi4HaHXBuUfNsi39nGKOCAAD42g9hllA236Z3robSR1cQdCHyrW
 export DB_USERNAME=Glass-Spades
 export DB_PASSWORD=Glassspades1m
-export DB_HOST=34.75.41.15
+export DB_HOST=34.139.164.249
 export DB_PORT=27017
 export TOKEN_KEY=Glassspades1m
 export GOOGLE_APPLICATION_CREDENTIALS="/home/richard/glass-spades/glass-spades-service-account.json"
@@ -14,6 +13,7 @@ export MAPS_API_KEY=AIzaSyDrVByoterVaoQyeLZ_ZmFvDBHcJJInQ84
 export GMAIL_APP_PASSWORD="nnqy jrrp wifm hmro"
 export PORT=443
 export HTTP_PORT=80
+export PATH="/home/richard/google-cloud-sdk/bin:/home/richard/.pyenv/shims:/home/richard/.pyenv/bin:/home/richard/.cargo/bin:/home/richard/.local/bin:/home/richard/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/richard/.rvm/bin:/home/richard/.rvm/bin"
 
-cd /home/richard/glass-spades
-node index.js
+node /home/richard/glass-spades/index2.js
+# node /home/richard/glass-spades/database/models.js
