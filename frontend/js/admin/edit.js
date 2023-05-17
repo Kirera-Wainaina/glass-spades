85498ec0ea45e998c95d122432a76862fb43e15f
Kirera Wainaina <richardwainainak@gmail.com>
2023-05-17 08:58:18 +0300

    indent frontend edit.js

--- frontend/js/admin/edit.js
+++ frontend/js/admin/edit.js
@@ -1,6 +1,12 @@
-import { startImageDrag, dragOverImage,
-	 dropImage, setData,
-	 getImages, confirmValues, highlightImage } from "./upload.js";
+import { 
+    startImageDrag, 
+    dragOverImage,
+	dropImage, 
+    setData,
+	getImages, 
+    confirmValues, 
+    highlightImage 
+} from "./upload.js";
 import { showLoadingPage, checkLogin } from "../general.js";
 
 if (navigator.userAgent != "glassspades-headless-chromium") {
@@ -19,40 +25,45 @@ function retrieveListing() {
     xhr.send(url.searchParams.get("id"));
 
     xhr.onreadystatechange = function() {
-	if (this.readyState == 4) {
-	    if (this.response != "fail") {
-		const data = JSON.parse(this.response);
-		console.log(data)
-		listingData = data.listing;
-		handleListing(data.listing);
-		displayImages(data.listingImages);
-		processCategory(data.listing);
+	    if (this.readyState == 4) {
+	        if (this.response != "fail") {
+	    	    const data = JSON.parse(this.response);
+	    	    console.log(data)
+	    	    listingData = data.listing;
+	    	    handleListing(data.listing);
+	    	    displayImages(data.listingImages);
+	    	    processCategory(data.listing);
+	        }
 	    }
-	}
     }
 }
 
 function handleListing(listing) {
     const keys = Object.keys(listing);
     keys.forEach(key => {
-	if (key == "_id" || key == "__v") {
-	    return ;
-	} else if (Array.isArray(listing[key])) {
-	    enterArrayData(key, listing[key]);
-	} else if (key == "Heading" || key == "Description" || key == "Price"
-		   || key == "Size" || key == "Development") {
-	    enterTypedData(key, listing[key]);
-	} else if (key == "Location") {
-	    if (listing[key].coordinates.length) {
-		const coords = listing.Location.coordinates;
-		const event = new Event("coords", { bubble: false })
-		sessionStorage.setItem("Latitude", coords[1]);
-		sessionStorage.setItem("Longitude", coords[0]);
-		document.dispatchEvent(event);
+	    if (key == "_id" || key == "__v") {
+	        return ;
+	    } else if (Array.isArray(listing[key])) {
+	        enterArrayData(key, listing[key]);
+	    } else if (
+            key == "Heading" || 
+            key == "Description" || 
+            key == "Price" ||
+	    	key == "Size" ||
+            key == "Development"
+        ) {
+	        enterTypedData(key, listing[key]);
+	    } else if (key == "Location") {
+	        if (listing[key].coordinates.length) {
+	    	    const coords = listing.Location.coordinates;
+	    	    const event = new Event("coords", { bubble: false })
+	    	    sessionStorage.setItem("Latitude", coords[1]);
+	    	    sessionStorage.setItem("Longitude", coords[0]);
+	    	    document.dispatchEvent(event);
+	        }
+	    } else {
+	        enterData(key, listing[key])
 	    }
-	} else {
-	    enterData(key, listing[key])
-	}
 	
     });
 }
@@ -69,19 +80,19 @@ function enterTypedData(key, value) {
 
 function enterArrayData(key, values) {
     values.forEach(value => {
-	const element = document.querySelector(`[name="${key}"][value="${value}"]`);
-	element.classList.add("clicked-button");
+	    const element = document.querySelector(`[name="${key}"][value="${value}"]`);
+	    element.classList.add("clicked-button");
     })
 }
 
 function processCategory(listing) {
     if (listing.Category == "Apartment") {
-	const description = document.getElementById("Development-container");
-	const size = document.getElementById("Size-container");
-	const unitType = document.getElementById("Unit-Type-container");
-	description.style.display = "grid";
-	size.style.display = "grid";
-	unitType.style.display = "grid";
+	    const description = document.getElementById("Development-container");
+	    const size = document.getElementById("Size-container");
+	    const unitType = document.getElementById("Unit-Type-container");
+	    description.style.display = "grid";
+	    size.style.display = "grid";
+	    unitType.style.display = "grid";
     }
 }
 
@@ -91,7 +102,7 @@ function displayImages(listingImages) {
     const imageArr = sortByPosition(listingImages);
 
     for (let i = 0; i < imageArr.length; i++) {
-	fragment.append(displayImage(imageArr[i]));
+	    fragment.append(displayImage(imageArr[i]));
     }
     dropZone.append(fragment);
 }
@@ -100,7 +111,8 @@ function sortByPosition(listingImages) {
     const imagePositions = listingImages.map(image => image.position);
     imagePositions.sort((a, b) => a - b);
     const sortedImages = imagePositions.map(imagePosition =>
-	listingImages.filter(image => image.position == imagePosition)[0])
+	    listingImages.filter(image => image.position == imagePosition)[0]
+    )
     return sortedImages
 }
 
@@ -130,12 +142,14 @@ async function organizeData() {
     showLoadingPage();
     
     clickedButtons.forEach(clickedButton => {
-	if (clickedButton.name == "External Features"
-	    || clickedButton.name == "Internal Features") {
-	    formdata.append(clickedButton.name, clickedButton.value)
-	} else {
-	    formdata.set(clickedButton.name, clickedButton.value);
-	}
+	    if (
+            clickedButton.name == "External Features" ||
+	        clickedButton.name == "Internal Features"
+        ) {
+	        formdata.append(clickedButton.name, clickedButton.value)
+	    } else {
+	        formdata.set(clickedButton.name, clickedButton.value);
+	    }
     })
 
     handleTypedData(formdata);
@@ -153,8 +167,8 @@ function handleCoordinates(formdata) {
 function handleTypedData(formdata) {
     const typedNames = ["Heading", "Description", "Price", "Size", "Development"];
     typedNames.forEach(typedName => {
-	const element = document.querySelector(`[name=${typedName}]`);
-	formdata.set(element.name, element.value)
+	    const element = document.querySelector(`[name=${typedName}]`);
+	    formdata.set(element.name, element.value)
     })
 }
 
@@ -164,16 +178,15 @@ async function handleFiles(formdata) {
 
     for (let i = 0; i < dropZone.childElementCount; i++) {
 	// JSON is not working so use querystring instead
-	const name = `name=${dropZone.children[i].id}&position=${i}`;
-	if (!dropZone.children[i].id.includes("-")) {
-	    formdata.append("fileId", name)
-	} else {
-	    filesInfo.push({ src: dropZone.children[i].src, name })
-	}
+	    const name = `name=${dropZone.children[i].id}&position=${i}`;
+	    if (!dropZone.children[i].id.includes("-")) {
+	        formdata.append("fileId", name)
+	    } else {
+	        filesInfo.push({ src: dropZone.children[i].src, name })
+	    }
     }
 
-    const files = await Promise.all(
-	filesInfo.map(info => createFileObject(info)));
+    const files = await Promise.all(filesInfo.map(info => createFileObject(info)));
     files.forEach(file => formdata.set(file.name, file.data));
 
     formdata.set("imageNum", files.length)
@@ -183,16 +196,16 @@ async function handleFiles(formdata) {
 
 function createFileObject(fileData) {
     return new Promise((resolve, reject) => {
-	const xhr = new XMLHttpRequest();
-	xhr.open("GET", fileData.src)
-	xhr.responseType = "blob";
-	xhr.send();
+	    const xhr = new XMLHttpRequest();
+	    xhr.open("GET", fileData.src)
+	    xhr.responseType = "blob";
+	    xhr.send();
 
-	xhr.onreadystatechange = function() {
-	    if (this.readyState == 4) {
-		resolve({ name: fileData.name, data: this.response})
+	    xhr.onreadystatechange = function() {
+	        if (this.readyState == 4) {
+	    	    resolve({ name: fileData.name, data: this.response})
+	        }
 	    }
-	}
     })
 }
 function updateListing(formdata) {
@@ -201,10 +214,10 @@ function updateListing(formdata) {
     xhr.send(formdata);
 
     xhr.onreadystatechange = function() {
-	if (this.readyState == 4) {
-	    if (this.response == "success") {
-		location.href = "/admin/listings";
+	    if (this.readyState == 4) {
+	        if (this.response == "success") {
+	    	    location.href = "/admin/listings";
+	        }
 	    }
-	}
     }
 }
