cd1d388e551dacc217e9fc1d2b174601c5872e71
Kirera Wainaina <richardwainainak@gmail.com>
2023-05-19 11:08:49 +0300

    indent general.js file

--- frontend/js/general.js
+++ frontend/js/general.js
@@ -23,33 +23,33 @@ function handleMenu(mediaQueryResult) {
     const menu = document.getElementById("menu");
     const menuButton = document.getElementById("menu-button");
     if (mediaQueryResult.matches) {
-	menu.style.display = "none";
-	menuButton.style.display = "block";
-	menuButton.addEventListener("click", showPhoneMenu);
-	document.addEventListener("click", hidePhoneMenu)
+	    menu.style.display = "none";
+	    menuButton.style.display = "block";
+	    menuButton.addEventListener("click", showPhoneMenu);
+	    document.addEventListener("click", hidePhoneMenu)
     } else {
-	menu.style.display = "flex";
-	menuButton.style.display = "none";
-	menuButton.removeEventListener("click", showPhoneMenu);
-	document.removeEventListener("click", hidePhoneMenu);
+	    menu.style.display = "flex";
+	    menuButton.style.display = "none";
+	    menuButton.removeEventListener("click", showPhoneMenu);
+	    document.removeEventListener("click", hidePhoneMenu);
     }
 }
 
 function showPhoneMenu(event) {
     const menu = document.getElementById("menu");
     if (menu.style.display == "none") {
-	menu.style.display = "flex";
-	menu.classList.add("phone-menu");
+	    menu.style.display = "flex";
+	    menu.classList.add("phone-menu");
     } else {
-	menu.style.display  = "none";
+	    menu.style.display  = "none";
     }
 }
 
 function hidePhoneMenu(event) {
     const menu = document.getElementById("menu");
     if (event.target.id != "menu-button" && menu.style.display == "flex") {
-	menu.style.display = "none";
-	console.log("called")
+	    menu.style.display = "none";
+	    console.log("called")
     }
 }
 
@@ -58,44 +58,44 @@ const exists = new Event("exists");
 document.addEventListener("exists", () => {
     const overviewImages = document.querySelectorAll(".overview");
     const options = {
-	root: null,
-	rootMargin: "0px",
-	threshold: .2
+	    root: null,
+	    rootMargin: "0px",
+	    threshold: .2
     };
 
     overviewImages.forEach(overview => {
- 	const observer = new IntersectionObserver(handleIntersect, options);
-	observer.observe(overview);
+ 	    const observer = new IntersectionObserver(handleIntersect, options);
+	    observer.observe(overview);
     })
 })
 
 function handleIntersect(entries) {
     entries.forEach(entry => {
-	if (entry.isIntersecting) {
-	    entry.target.src = entry.target.dataset.imageSrc;
-	}
+	    if (entry.isIntersecting) {
+	        entry.target.src = entry.target.dataset.imageSrc;
+	    }
     })
 }
 
 export function displayHouseDetails(houseDetails, containerId="listings") {
     if (!document.querySelector(".house-card")) {
-	// before or without ssr
-	const listings = document.getElementById(containerId);
-	const fragment = new DocumentFragment();
+	    // before or without ssr
+	    const listings = document.getElementById(containerId);
+	    const fragment = new DocumentFragment();
 
-	houseDetails.forEach(houseDetail => {
-	    const houseCard = createHouseCard(houseDetail);
-	    fragment.append(houseCard);
-	})
+	    houseDetails.forEach(houseDetail => {
+	        const houseCard = createHouseCard(houseDetail);
+	        fragment.append(houseCard);
+	    })
 
-	if (listings) listings.appendChild(fragment);
-	// dispatch event after the images are in dom
-	if (navigator.userAgent != "glassspades-headless-chromium") {
-	    // dont dispatch for ssr
-	    document.dispatchEvent(exists);
-	}
+	    if (listings) listings.appendChild(fragment);
+	    // dispatch event after the images are in dom
+	    if (navigator.userAgent != "glassspades-headless-chromium") {
+	        // dont dispatch for ssr
+	        document.dispatchEvent(exists);
+	    }
     } else {
-	document.dispatchEvent(exists);
+	    document.dispatchEvent(exists);
     }
 }
 
@@ -148,13 +148,19 @@ function createOverviewDetails(houseDetail) {
     container.id = "overview-details";
     container.append(inputPrice(houseDetail.price));
     if (houseDetail.category == "Land") {
-	container.append(createIcon(houseDetail.size,
-				    "/frontend/images/size-icon.svg"))
+	    container.append(createIcon(
+            houseDetail.size,
+	    	"/frontend/images/size-icon.svg"
+        ))
     } else {
-	container.append(createIcon(houseDetail.bedrooms,
-				    "/frontend/images/bed-icon.svg"))
-	container.append(createIcon(houseDetail.bathrooms,
-				    "/frontend/images/bath-icon.svg"))
+	    container.append(
+            createIcon(houseDetail.bedrooms,
+			"/frontend/images/bed-icon.svg"
+        ))
+	    container.append(
+            createIcon(houseDetail.bathrooms,
+		    "/frontend/images/bath-icon.svg"
+        ))
     }
     return container
 }
@@ -191,27 +197,27 @@ export function hideLoadingPage() {
 if (location.pathname == "/sales" || location.pathname == "/rentals") {
     const locationElement = document.getElementById("locations");
     if (locationElement.childElementCount <= 1) {
-	(function (){
-	    // Get Locations
-	    const xhr = new XMLHttpRequest();
-	    xhr.open("GET", "/api/admin/upload/sendModelData");
-	    xhr.send()
+	    (function (){
+	        // Get Locations
+	        const xhr = new XMLHttpRequest();
+	        xhr.open("GET", "/api/admin/upload/sendModelData");
+	        xhr.send()
 
-	    xhr.onreadystatechange = function() {
-		if (this.readyState == 4) {
-		    const model = JSON.parse(this.response);
-		    const locationNames = model["Location Name"];
-		    const fragment = new DocumentFragment();
-		    locationNames.forEach(locationName => {
-			const option = document.createElement("option");
-			option.value = locationName;
-			option.textContent = locationName;
-			fragment.append(option)
-		    })
-		    locationElement.appendChild(fragment);
-		}
-	    }
-	})()
+	        xhr.onreadystatechange = function() {
+	    	    if (this.readyState == 4) {
+	    	        const model = JSON.parse(this.response);
+	    	        const locationNames = model["Location Name"];
+	    	        const fragment = new DocumentFragment();
+	    	        locationNames.forEach(locationName => {
+	    	    	    const option = document.createElement("option");
+	    	    	    option.value = locationName;
+	    	    	    option.textContent = locationName;
+	    	    	    fragment.append(option)
+	    	        })
+	    	        locationElement.appendChild(fragment);
+	    	    }
+	        }
+	    })()
     }
 }
 
@@ -245,14 +251,14 @@ export function runFilter(mandate) {
     }
 
     if (verifyPrices(maxPrice, minPrice)) {
-	queries.push(`max-price=${maxPrice}`);
-	queries.push(`min-price=${minPrice}`);
+	    queries.push(`max-price=${maxPrice}`);
+	    queries.push(`min-price=${minPrice}`);
     }
     if (verifyValue(bedrooms)) {
-	queries.push(`bedrooms=${bedrooms}`);
+	    queries.push(`bedrooms=${bedrooms}`);
     }
     if (verifyValue(location)) {
-	queries.push(`location=${location}`);
+	    queries.push(`location=${location}`);
     }
 
     closeIcon.click();
@@ -261,16 +267,16 @@ export function runFilter(mandate) {
 
 function verifyPrices(maxPrice, minPrice) {
     if (Number(maxPrice) < Number(minPrice)) {
-	const notice = document.getElementById("wrong-entry");
-	notice.style.display = "block";
-	return false
+	    const notice = document.getElementById("wrong-entry");
+	    notice.style.display = "block";
+	    return false
     }
     return true
 }
 
 function verifyValue(value) {
     if (value == "") {
-	return false
+	    return false
     }
     return true
 }
@@ -278,56 +284,46 @@ function verifyValue(value) {
 function handleRedirection(filterNotices, queries) {
     let query = "";
     queries.forEach((q, index, array) => {
-	if (index != array.length - 1) {
-	    query += `${q}&`;
-	} else {
-	    // last query doesn't have query separator '&'
-	    query += `${q}`;
-	}
+	    if (index != array.length - 1) {
+	        query += `${q}&`;
+	    } else {
+	        // last query doesn't have query separator '&'
+	        query += `${q}`;
+	    }
     });
     
     for (let i = 0; i < filterNotices.length; i++) {
-	if (filterNotices[i].style.display == "block") {
-	    break
-	} else if (i == filterNotices.length - 1) {
-	    location.href = location.origin + location.pathname + "?"+ query;
-	}
+	    if (filterNotices[i].style.display == "block") {
+	        break
+	    } else if (i == filterNotices.length - 1) {
+	        location.href = location.origin + location.pathname + "?"+ query;
+	    }
     }
 }
 
 export function generateTitle() {
     let title = "";
     if (location.search) {
-	const params = new URLSearchParams(location.search);
-	const maxPrice = params.get("max-price");
-	const minPrice = params.get("min-price");
-	if (params.has("bedrooms")) {
-	    const bedrooms = params.get("bedrooms");
-	    if (bedrooms == "Studio") {
-		title += `${bedrooms} `;
-	    } else {
-		title += `${bedrooms} bedroom `;
+	    const params = new URLSearchParams(location.search);
+	    const maxPrice = params.get("max-price");
+	    const minPrice = params.get("min-price");
+	    if (params.has("bedrooms")) {
+	        const bedrooms = params.get("bedrooms");
+	        if (bedrooms == "Studio") {
+	    	    title += `${bedrooms} `;
+	        } else {
+	    	    title += `${bedrooms} bedroom `;
+	        }
 	    }
-	}
-	title += "Houses ";
-	const mandate = location.pathname.replace("/", "");
-	if (mandate == "sales") {
-	    title += `for sale `;
-	} else {
-	    title += `for rent `;
-	}
+	    title += "Houses ";
+	    const mandate = location.pathname.replace("/", "");
+	    if (mandate == "sales") {
+	        title += `for sale `;
+	    } else {
+	        title += `for rent `;
+	    }
 
-	if (params.has("location")) title += `in ${params.get("location")} `;
-
-	// title += "with price "
-
-	// if (minPrice && maxPrice == Number.MAX_SAFE_INTEGER) {
-	//     title += `above ${minPrice} `;
-	// } else if (!minPrice && maxPrice != Number.MAX_SAFE_INTEGER) {
-	//     title += `below ${maxPrice} `;
-	// } else if (minPrice && maxPrice != Number.MAX_SAFE_INTEGER) {
-	//     title += `between ${minPrice} and ${maxPrice}`;
-	// }
+	    if (params.has("location")) title += `in ${params.get("location")} `;
     }
     return title
 }
@@ -338,9 +334,9 @@ export function checkLogin() {
     xhr.send()
 
     xhr.onreadystatechange = function() {
-	if (xhr.readyState == 4 && xhr.response == "fail") {
-	    location.href = "/login"
-	}
+	    if (xhr.readyState == 4 && xhr.response == "fail") {
+	        location.href = "/login"
+	    }
     }
 }
 
