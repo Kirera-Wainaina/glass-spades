8138d142331ee170f28038e0b1f144dd28afd006
Kirera Wainaina <richardwainainak@gmail.com>
2023-05-24 22:45:49 +0300

    use if statement to check if url is a filter url

--- api/sales.js
+++ api/sales.js
@@ -1,7 +1,22 @@
 const utils = require("../utils/general");
+const dotenv = require('dotenv');
+dotenv.config();
 
 function getSales(request, response) {
+    handleFilterUrls(request)
     utils.getListings(request, response)
+}
+
+function handleFilterUrls(request) {
+    const parsedUrl = new URL(request.url, process.env.URL);
+    console.log(parsedUrl)
+    if (parsedUrl.search) {
+        // check if it's a filter url
+        // check if rendered page exists in static file
+        // if not, trigger rendering
+        // if it does, simply return
+    }
+    
 }
 
 exports.getSales = getSales;
