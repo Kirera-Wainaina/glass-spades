450ef0853267a0746b1d9071b35651897b06d8c4
Kirera Wainaina <richardwainainak@gmail.com>
2023-05-24 23:10:41 +0300

    check if filtered file exists before rendering

--- api/sales.js
+++ api/sales.js
@@ -1,5 +1,7 @@
 const utils = require("../utils/general");
 const dotenv = require('dotenv');
+const fsPromises = require('fs/promises');
+const path = require('path');
 dotenv.config();
 
 function getSales(request, response) {
@@ -7,7 +9,7 @@ function getSales(request, response) {
     utils.getListings(request, response)
 }
 
-function handleFilterUrls(request) {
+async function handleFilterUrls(request) {
     const parsedUrl = new URL(request.url, process.env.URL);
     console.log(parsedUrl)
     if (parsedUrl.search) {
@@ -15,8 +17,29 @@ function handleFilterUrls(request) {
         // check if rendered page exists in static file
         // if not, trigger rendering
         // if it does, simply return
+        const fileName = parsedUrl.search.replace('?', '');
+        const filePath = path.join(__dirname, '..', 'static', 'sales-filters', fileName);
+        if (await fileExists(filePath)) {
+            return ;
+        } else {
+            // render page
+        }
     }
     
+}
+
+async function fileExists(filePath) {
+    let fileExists = null;
+    await fsPromises.open(filePath)
+        .then(fileHandle => {
+            if (fileHandle) fileExists = true;
+        })
+        .catch(error => {
+            if (error.code == 'ENOENT') {
+                fileExists = false;
+            }
+        });
+    return fileExists
 }
 
 exports.getSales = getSales;
