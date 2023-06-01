57c9e764c59d8e1c966b5ba8bb34905f5b55516d
Kirera Wainaina <richardwainainak@gmail.com>
2023-06-01 07:49:30 +0300

    close the image upload modal when close icon is clicked

--- frontend/js/admin/article-images.js
+++ frontend/js/admin/article-images.js
@@ -1,9 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
   const cloudUploadIcon = document.getElementById('cloud-upload-icon');
-  cloudUploadIcon.addEventListener('click', showImageUploadModal);
+  cloudUploadIcon.addEventListener('click', toggleImageUploadModal);
+
+  const closeUploadModal = document.getElementById('close-upload-modal');
+  closeUploadModal.addEventListener('click', toggleImageUploadModal)
 })
 
-function showImageUploadModal() {
+function toggleImageUploadModal() {
   const imageUploadModal = document.getElementById('image-upload-modal');
   imageUploadModal.classList.toggle('flex');
 }
