9f30acf2aaa809ae1e0213607a9c88313c19b530
Kirera Wainaina <richardwainainak@gmail.com>
2023-06-01 08:55:33 +0300

    iterate through the filelist

--- frontend/js/admin/article-images.js
+++ frontend/js/admin/article-images.js
@@ -7,6 +7,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const uploadButton = document.getElementById('upload-button');
   uploadButton.addEventListener('click', clickFileInput);
+
+  const fileInput = document.getElementById('form input[type="file"]');
+  fileInput.addEventListener('change', previewImagesToUpload)
 })
 
 function toggleImageUploadModal() {
@@ -17,4 +20,18 @@ function toggleImageUploadModal() {
 function clickFileInput() {
   const fileInput = document.querySelector("input[name='articleImages']");
   fileInput.click();
+}
+
+function previewImagesToUpload(event) {
+  const files = event.target.files;
+  const fragment = new DocumentFragment();
+
+  for (const file of files) {
+    fragment.append(createImagePreviewContainer(file))
+  }
+}
+
+function createImagePreviewContainer(file) {
+  const div = document.createElement('div');
+  return div;
 }
