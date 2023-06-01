2720982389067e7f76117cb026fcf5c624e2f026
Kirera Wainaina <richardwainainak@gmail.com>
2023-06-01 12:13:39 +0300

    save image to cloud and retrieve metadata

--- api/admin/article-images.js
+++ api/admin/article-images.js
@@ -1,8 +1,24 @@
 const FormDataHandler = require("../../utils/formDataHandler");
+const { minifyImage, saveImage } = require("../../utils/images");
 
 async function uploadImages(request, response) {
   const [fields, files] = await new FormDataHandler(request).run();
-  console.log(files);
+
+  const convertedFileMetadata = await Promise.all(
+    files.map(filePath => minifyImage(filePath))
+  ).then(data => data.flat());
+
+  const cloudFiles = await Promise.all(
+    convertedFileMetadata.map(metadata => saveImage(metadata.destinationPath))
+  )
+  
+  const cloudMetadata = await Promise.all(
+    cloudFiles.map(file => {
+      return file.getMetadata()
+        .then(data => data[0])
+    })
+  );
+  console.log(cloudMetadata);
 }
 
 exports.uploadImages = uploadImages;
