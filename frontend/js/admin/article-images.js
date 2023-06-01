ca30a85870e6f32fd375ca1985516037d7af6363
Kirera Wainaina <richardwainainak@gmail.com>
2023-06-01 08:15:03 +0300

    open file input when upload button is clicked

--- frontend/js/admin/article-images.js
+++ frontend/js/admin/article-images.js
@@ -3,10 +3,18 @@ document.addEventListener('DOMContentLoaded', () => {
   cloudUploadIcon.addEventListener('click', toggleImageUploadModal);
 
   const closeUploadModal = document.getElementById('close-upload-modal');
-  closeUploadModal.addEventListener('click', toggleImageUploadModal)
+  closeUploadModal.addEventListener('click', toggleImageUploadModal);
+
+  const uploadButton = document.getElementById('upload-button');
+  uploadButton.addEventListener('click', clickFileInput);
 })
 
 function toggleImageUploadModal() {
   const imageUploadModal = document.getElementById('image-upload-modal');
   imageUploadModal.classList.toggle('flex');
+}
+
+function clickFileInput() {
+  const fileInput = document.querySelector("input[name='articleImages']");
+  fileInput.click();
 }
