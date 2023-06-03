98005509a2af6128f2e6f3927b7db0b0466fc13c
Kirera Wainaina <richardwainainak@gmail.com>
2023-06-03 09:04:20 +0300

    save author to database and send response

--- api/admin/add-author.js
+++ api/admin/add-author.js
@@ -1,6 +1,9 @@
 const FormDataHandler = require('../../utils/formDataHandler');
+const db = require('../../database/models');
 const bcrypt = require('bcrypt')
 const dotenv = require('dotenv');
+const fsPromises = require('fs/promises')
+const { minifyImage, saveImage } = require('../../utils/images');
 dotenv.config()
 
 exports.saveAuthor = async function (request, response) {
@@ -9,8 +12,22 @@ exports.saveAuthor = async function (request, response) {
     const result = await bcrypt.compare(fields.adminPassword, process.env.ENCRYPTED_ADMIN_PASSWORD);
 
     if (result) {
-      console.log(files, fields);
-      
+      const [ convertedFileMetadata ] = await minifyImage(files[0]);
+      const cloudFile = await saveImage(convertedFileMetadata.destinationPath);
+      const [ cloudMetadata ] = await cloudFile.getMetadata();
+
+      await db.Author.create({
+        ...fields,
+        profileImageLink: cloudMetadata.mediaLink,
+        profileImageName: cloudMetadata.name
+      })
+      await Promise.all([
+        fsPromises.unlink(convertedFileMetadata.destinationPath), 
+        fsPromises.unlink(convertedFileMetadata.sourcePath)
+      ]);
+      console.log('successfully save author')
+      response.writeHead(200, { 'content-type': 'text/plain' })
+      response.end('success');
     } else {
       response.writeHead(401, {'content-type': 'text/plain'});
       response.end('forbidden-error')
