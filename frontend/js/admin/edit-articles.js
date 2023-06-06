0862c2757401487e71e5ae1026601eea85a0bebf
Kirera Wainaina <richardwainainak@gmail.com>
2023-06-06 07:13:21 +0300

    delete previous retrieveArticleData function

--- frontend/js/admin/edit-articles.js
+++ frontend/js/admin/edit-articles.js
@@ -2,18 +2,6 @@ import { displayArticleList } from "../article-list.js";
 
 document.addEventListener('DOMContentLoaded', retrieveArticleData);
 
-function retrieveArticleData_() {
-    fetch('/api/articles?field=title&field=description\
-&field=landscapeImage&field=publishedDate&field=urlTitle\
-&orderBy=publishedDate&orderByDirection=desc')
-        .then(response => response.json())
-        .then(data => displayArticleList(
-            document.getElementById('article-list'),
-            data,
-            createEditHref
-        ))
-}
-
 function retrieveArticleData() {
   fetch("/api/admin/edit-articles/retrieveArticleData")
     .then(response => response.json())
