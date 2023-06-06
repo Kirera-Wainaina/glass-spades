7785f89607f7a77c994059c463370967707562b4
Kirera Wainaina <richardwainainak@gmail.com>
2023-06-06 23:13:07 +0300

    remove a log line

--- api/articles.js
+++ api/articles.js
@@ -19,7 +19,6 @@ exports.getArticles = async function (request, response) {
       .sort({ publishedDate: "desc" })
       .limit(Number(parsed_url.searchParams.get("limit")))
       .skip(Number(parsed_url.searchParams.get("offset")));
-      console.log(docs)
     
     response.writeHead(200, { "content-type": "application/json"});
     response.end(JSON.stringify(docs));
