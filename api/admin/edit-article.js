eccaedf008f31668585ceb208a9296d83c14eff5
Kirera Wainaina <richardwainainak@gmail.com>
2023-06-06 17:04:43 +0300

    edit article and save

--- api/admin/edit-article.js
+++ api/admin/edit-article.js
@@ -1,5 +1,6 @@
 const dotenv = require("dotenv");
 const db = require("../../database/models");
+const FormDataHandler = require("../../utils/formDataHandler");
 
 dotenv.config();
 
@@ -12,6 +13,20 @@ exports.retrieveArticle = async function (request, response) {
     });
     response.writeHead(200, {"content-type": "application/json"});
     response.end(JSON.stringify(docs));
+  } catch (error) {
+    console.log(error);
+    response.writeHead(500, {"content-type": "text/plain"});
+    response.end("server-error");
+  }
+}
+
+exports.editArticle = async function (request, response) {
+  try {
+    const [fields] = await new FormDataHandler(request).run();
+    await db.Article.findByIdAndUpdate(fields.id, fields);
+    console.log("successfully edited article")
+    response.writeHead(200, {"content-type": "text/plain"});
+    response.end("success");
   } catch (error) {
     console.log(error);
     response.writeHead(500, {"content-type": "text/plain"});
