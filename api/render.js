24fb30b4e6256110aaeb291faf55c2b903e1328a
Kirera Wainaina <richardwainainak@gmail.com>
2023-06-08 19:18:29 +0300

    send related article data

--- api/render.js
+++ api/render.js
@@ -3,10 +3,20 @@ const db = require("../database/models");
 
 dotenv.config();
 
-exports.fetchRelatedArticleData = function (request, response) {
+exports.fetchRelatedArticleData = async function (request, response) {
   try {
     const parsed_url = new URL(request.url, process.env.URL);
-    
+    const docs = await db.Article.findById(
+      parsed_url.searchParams.get("id"), 
+      {
+        title: 1,
+        description: 1,
+        landscapeImage: 1,
+        publishedDate: 1
+      }
+    );
+    response.writeHead(200, {"content-type": "application/json"});
+    response.end(JSON.stringify(docs));
   } catch (error) {
     console.log(error);
     response.writeHead(500, {"content-type": "text/plain"});
