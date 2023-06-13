0b57ddaccc24e04fe66a69841458915af6e9d503
Kirera Wainaina <richardwainainak@gmail.com>
2023-06-13 19:11:14 +0300

    correct the function to upload sitemaps

--- api/admin/general-functions.js
+++ api/admin/general-functions.js
@@ -1,27 +1,27 @@
 const fs = require("fs");
+const fsPromises = require('fs/promises');
 const path = require("path");
 
-const Busboy = require("busboy");
 const dotenv = require("dotenv");
 
 const respond = require("../../utils/respond.js");
-const indexUtils = require("../../index-utils");
+const FormDataHandler = require("../../utils/formDataHandler.js");
+
 dotenv.config()
 
-function uploadSitemap(request, response) {
-    const busboy = new Busboy({ headers: request.headers });
-
-    busboy.on("file", (name, file) => {
-	file.pipe(fs.createWriteStream(process.env.SITEMAP))
-	    .on("error", error => console.log(error))
-    })
-
-    busboy.on("finish", () => {
-	indexUtils.routeCache.delete(`/sitemap.xml`)
-	respond.handleTextResponse(response, "success");
-    })
-
-    request.pipe(busboy);
+exports.uploadSitemap = async function (request, response) {
+    try {
+        const [fields, files] = await new FormDataHandler(request).run();
+        const sitemapPath = path.join(__dirname, '..', '..', "sitemap.xml");
+        fs.createReadStream(files[0])
+            .pipe(fs.createWriteStream(sitemapPath))
+            .on("finish", async () => {
+                await fsPromises.unlink(files[0]);
+            	respond.handleTextResponse(response, "success");
+            })
+    } catch (error) {
+        console.log(error);
+        response.writeHead(500, { "content-type": "text/plain"});
+        response.end("server-error");
+    }
 }
-
-exports.uploadSitemap = uploadSitemap;
