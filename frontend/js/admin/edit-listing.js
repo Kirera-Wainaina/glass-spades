5494ffb7b817025fbd4a36c888ae4705109c197a
Kirera Wainaina <richardwainainak@gmail.com>
2023-06-18 10:50:17 +0300

    save completion month and display it

--- frontend/js/admin/edit-listing.js
+++ frontend/js/admin/edit-listing.js
@@ -55,6 +55,8 @@ function handleListing(listing) {
 	    	    sessionStorage.setItem("Longitude", coords[0]);
 	    	    document.dispatchEvent(event);
 	        }
+        } else if (key == "Completion Month") {
+            enterCompletionMonth(listing[key])
 	    } else {
 	        enterData(key, listing[key])
 	    }
@@ -77,6 +79,11 @@ function enterArrayData(key, values) {
 	    const element = document.querySelector(`[name="${key}"][value="${value}"]`);
         if (element) element.classList.add("clicked-button");
     })
+}
+
+function enterCompletionMonth(value) {
+    const select = document.querySelector("select[name='Completion Month']");
+    select.value = value;
 }
 
 function processCategory(listing) {
@@ -133,6 +140,7 @@ window.addEventListener("DOMContentLoaded", () => {
 async function organizeData() {
     let formdata = new FormData();
     const clickedButtons = document.querySelectorAll(".clicked-button");
+    const completionMonth = document.querySelector(`select[name="Completion Month"]`).value;
     showLoadingPage();
     
     clickedButtons.forEach(clickedButton => {
@@ -148,6 +156,7 @@ async function organizeData() {
 
     handleTypedData(formdata);
     formdata.set("id", listingData._id);
+    formdata.set("Completion Month", completionMonth)
     handleCoordinates(formdata);
     await handleFiles(formdata);
 
